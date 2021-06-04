46b8d28c07deed0aad8c347c119f5f0bba2990f6
BrunaTBarreto <brunatrebienbarreto@outlook.com>
2021-06-04 10:07:20 -0300

    Project created

---
+++ backend/src/main/java/com/desenvolvimento/capcatalog/CapcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.desenvolvimento.capcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapcatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/desenvolvimento/capcatalog/CapcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desenvolvimento.capcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
